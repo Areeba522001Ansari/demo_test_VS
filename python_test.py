c6c3a5893eb57375149596186462ec032e61242f
Areeba <kmareeba2001@gmail.com>
2024-12-30 13:49:59 +0530

    removed code and function

--- python_test.py
+++ python_test.py
@@ -1,16 +1,21 @@
-print("hello world")
-print(sum(10,9))
-#streamlit code
-print("Areeba")
-print("Affine")
-print("interns")
-a=10
-b=20
-print(sum(a,b))
-a=1
-b=2
-print(sum(a,b))
-a=1
-b=2
-c=a+b
-print(c)
+# print("hello world")
+# print(sum(10,9))
+# #streamlit code
+# print("Areeba")
+# print("Affine")
+# print("interns")
+# a=10
+# b=20
+# print(sum(a,b))
+# a=1
+# b=2
+# print(sum(a,b))
+# a=1
+# b=2
+# c=a+b
+# print(c)
+
+def add(a, b):
+    return a + b
+
+print(add(4, 5))
